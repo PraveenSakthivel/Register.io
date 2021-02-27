c9d4642cde816635abd08d2110cf54170b632e2c
Praveen Sakthivel <prsakthivel089@gmail.com>
2021-02-26 23:42:00 -0500

    Completed Consumer

--- CourseValidation/Main/cv.go
+++ CourseValidation/Main/cv.go
@@ -2,15 +2,20 @@ package main
 
 import (
 	"context"
+	"errors"
 	"flag"
 	"log"
 	"net"
+	prereqInterface "registerio/cv/main/prereq"
 	cvInterface "registerio/cv/main/protobuf"
+	"time"
 
 	"google.golang.org/grpc"
 )
 
 var debug = false
+
+const PREREQ_ENDPOINT = ":8081"
 
 //Server instance
 type Server struct {
@@ -20,7 +25,7 @@ type Server struct {
 //Student representation for handling request
 type Student struct {
 	netID        string
-	classHistory map[string]string
+	classHistory map[string]int32
 }
 
 //debug print
@@ -38,7 +43,51 @@ func NewServer() *Server {
 
 //Dummy function, replace with imported library later
 func parseJWT(token string) (Student, error) {
-	return Student{netID: "ps931", classHistory: {"198:101": "A", "198:102", "A"}}, nil
+	return Student{netID: "ps931", classHistory: {"198:101": 40, "198:102", 40}}, nil
+}
+
+func checkPrereqs(classHistory map[string]int32, indices []string) (*prereqInterface.PrereqResponse, error) {
+	prereq := prereqInterface.PrereqRequest{ClassHistory: classHistory, Indices: indices}
+	var preResult *prereqInterface.PrereqResponse
+	var conn *grpc.ClientConn
+	var err error
+	start := time.Now()
+	for err != nil {
+		conn, err = grpc.Dial(PREREQ_ENDPOINT, grpc.WithInsecure())
+		if err != nil {
+			log.Panic("ERROR: Unable to connect to Prereq Endpoint: ", err)
+			continue
+		}
+		server := prereqInterface.NewPrereqValidationClient(conn)
+		ctx, _ := context.WithTimeout(context.Background(), 200*time.Millisecond)
+		preResult, err = server.CheckPrereqs(ctx, &prereq)
+		if err != nil {
+			log.Panic("ERROR: Unable to connect to Prereq Endpoint: ", err)
+		} else if time.Now().Sub(start) > 1000 {
+			return nil, errors.New("Timeout")
+		}
+	}
+	conn.Close()
+	return preResult, nil
+}
+
+func evalPrereqResults(preResult *prereqInterface.PrereqResponse, results *map[string]cvInterface.ResultClass) []string {
+	var eligibleClasses []string
+	resultMap := *results
+
+	for _, index := range preResult.InvalidIndices {
+		resultMap[index] = cvInterface.ResultClass_INVALID
+	}
+
+	for index, status := range preResult.Results {
+		if status == false {
+			resultMap[index] = cvInterface.ResultClass_PREREQ
+		} else {
+			eligibleClasses = append(eligibleClasses, index)
+			resultMap[index] = cvInterface.ResultClass_OK
+		}
+	}
+	return eligibleClasses
 }
 
 func (s *Server) ChangeRegistration(ctx context.Context, req *cvInterface.RegistrationRequest) (*cvInterface.RegistrationResponse, error) {
@@ -52,8 +101,18 @@ func (s *Server) ChangeRegistration(ctx context.Context, req *cvInterface.Regist
 
 	student, err := parseJWT(req.Token)
 	if err != nil {
+		log.Panic("ERROR Parsing JWT")
 		return &response, nil
 	}
+
+	preResult, err := checkPrereqs(student.classHistory, indices)
+	if err != nil {
+		log.Panic("ERROR Timed out trying to connect to prereq endpoint")
+		return &response, nil
+	}
+
+	eligibleClasses := evalPrereqResults(preResult, &results)
+
 }
 
 func main() {
